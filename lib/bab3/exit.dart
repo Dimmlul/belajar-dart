a9d862112e1b88ca3bb4e20e6952ef86941e493a
Dimm <dimaspradiptam@gmail.com>
2024-10-23 11:25:31 +0700

    23/10/2024

---
+++ lib/bab3/exit.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+ print('perintah 1');
+ print('perintah 2');
+
+
+
+
+ print('perintah 3');
+ print('perintah 4');
+ print('perintah 5');
+  exit(1); // menghentikan program secara paksa
+ 
+}
